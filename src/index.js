4d509d4dca4c0a766a9273c6e5d34fe575705a33
Hyan Ventura <hyanventura12@hotmail.com>
2025-05-21 16:59:10 -0300

    feat: adiciona bootstrap ao projeto e antdesign

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'antd/dist/reset.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
